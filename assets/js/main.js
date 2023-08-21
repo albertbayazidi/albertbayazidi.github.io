8b8ecd52ab4bd5657f43194f65875f95c2ebe70a
Albert <albert.bayazidi@gmail.com>
2023-08-21 20:51:49 +0200

    la til endringer som gjør det mulig med typewriter effect. og easteregg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,34 @@
+const currentUrl = window.location.href;
+const textArray = currentUrl.split("/")
+const word = textArray[textArray.length - 2]
+const possibleText = "Meow"
+const txt = 'Home/' + word;
+
 var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
+var speed = 100;
+var payload = txt;
 var backspace = false;
+var completed = 0;
 
-var outputElem = document.getElementById("demo"); // get element when page loads
+var outputElem = document.getElementById("typeWriter"); // get element when page loads
+
 function typeWriter() {
-  outputElem.textContent = txt.substring(0, i);
-
-  if (i > txt.length) backspace = true;
-  if (i == 0) backspace = false;
-  i = i + (backspace ? -1 : 1);
-  setTimeout(typeWriter, speed);
+  if (completed < 3){
+    outputElem.textContent = payload.substring(0, i);
+    if (i > payload.length){
+      backspace = true;
+      completed ++;
+    }
+    
+    if (i == txt.length & payload == txt)speed = 5000;
+    if (i == 1 & payload == possibleText)payload = txt;
+    
+    if (completed == 1 & i == 1)payload = possibleText;
+  
+  
+    if (i == 0) backspace = false;
+    i = i + (backspace ? -1 : 1);
+    setTimeout(typeWriter, speed);
+    if (i == txt.length-1 & payload == txt)speed =50;
+  }
 }
